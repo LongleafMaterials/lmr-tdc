74f6c259f3463c36486b352c83fec09742d0b830
colin-lmr <colin@longleafresearch.com>
2024-07-29 20:57:58 -0400

    Add files via upload

---
+++ tdb_reader.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Read and parse thermodynamic database (.TDB) files
+
+Created on Mon Jul 29 20:46:37 2024
+
+@author: Colin-LMR
+"""
+
+### Initial work performed locally
+path = 'C:\\Users\\Colin-LMR\\OneDrive - Longleaf Materials Research\\Documents\\Longleaf Materials Research\\Projects\\lmr-tdc\\'
