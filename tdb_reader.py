e83da7aa78b8d8aecebf42d4671bc5b59a915f3b
colin-lmr <colin@longleafresearch.com>
2024-08-01 08:26:00 -0400

    Functioning reader

--- tdb_reader.py
+++ tdb_reader.py
@@ -16,10 +16,6 @@ import pandas as pd
 import re
 import json
 
-# Retrieve COST507R TDB file
-url = 'https://raw.githubusercontent.com/colin-lmr/lmr-tdc/main/tdb/cost507R.TDB'
-tdb = requests.get(url).text
-
 ## Clean data
 def cleanTDB(tdb):
     # Split tdb into lines by '\n'
@@ -41,6 +37,15 @@ def cleanTDB(tdb):
     return tdbLines
 
 ### Extract elements, phase names, and parameters
+# Data structure for a single element entry:
+# {
+# 'element': <element name>,
+# 'phase': <phase name>,
+# 'atomic_mass': <atomic mass of element>,
+# 'H298-H0': <standard enthalpy relative to 298K for element>,
+# 'S298': <standard absolute entropy for element>
+# }
+#
 def getElements(tdbLines):
     # Extract lines containing element data
     tdbLines = [line[8:] for line in tdbLines if line[:7] == 'ELEMENT']
@@ -70,6 +75,15 @@ def getElements(tdbLines):
     return dataStruct
 
 ### Extract functions
+# Data structure for a single function entry:
+# {
+# 'name': <function name>,
+# 'functions': [{'function': <calculation function>,      
+#                'max_temp': <upper temperature bound for function>,
+#                'min_temp': <lower temperature bound for function>},
+#               ...]
+# }
+#
 def getFunctions(tdbLines):
     # Remove "FUNCT" and "FUNCTION" from beginning of lines (may not be necessary)
     tdbLines = [x.replace('FUNCT ', 'FUNCTION ')[9:] for x in [line.replace('\n','') for line in tdbLines if line[:5] == 'FUNCT']]
@@ -110,6 +124,15 @@ def getFunctions(tdbLines):
     return dataStruct
 
 ### Extract parameters
+# Data structure for a single parameter entry:
+# {
+# 'name': <parameter name>,
+# 'parameters': [{'function': <calculation function>,      
+#                 'max_temp': <upper temperature bound for function>,
+#                 'min_temp': <lower temperature bound for function>},
+#                ...]
+# }
+#
 def getParameters(tdbLines):
     # Remove "PARAMETER" from beginning of lines
     tdbLines = [x.replace('PARAM ', 'PARAMETER ')[10:] for x in [line.replace('\n','') for line in tdbLines if line[:5] == 'PARAM']]
@@ -151,47 +174,86 @@ def getParameters(tdbLines):
 ### Extract phases and constituents
 # Data structure for a single phase entry:
 #   {
-#   phase: <phasename>
-#   sublattices: <number of sublattices>
-#   stoichiomery: [<stoichiometry for each sublattice>]
-#   constituent: <constituent name>
-#   constituent: <constituent name>
-#   ...
+#   'phase': <phasename>,
+#   'sublattices': <number of sublattices>,
+#   'stoichiomery': [<stoichiometry for each sublattice>],
+#   'constituents': [{'constituent': <element(s)>},
+#                    {'constituent': <element(s)>},
+#                    ...]
 #   }
-#def getPhases(tdbLines):
-tdbLines = cleanTDB(tdb) # TEMPORARY UNTIL FUNCTION IS BUILT
-# Reduce list to rows which contain phases and constituents
-tdbLines = [r for r in tdbLines if any([r[:5] == 'PHASE', r[:5] == 'CONST'])]
-
-# Initialize data structure
-dataStruct = []
-
-# Lines defining phase and constituents appear in pairs, with CONSTITUENT line
-# following a PHASE line. Isolate each type and then process in pairs.
-phases = [tdbLines[i][5:].strip() for i in range(0,len(tdbLines),2)]
-constituents = [tdbLines[i][11:].strip() for i in range(1,len(tdbLines),2)] 
-for p, c in zip(phases, constituents):
-    # Split phase information into list
-    phaseList = [i for i in p.split(' ') if len(i)>0]
+#
+def getPhases(tdbLines):
+    # Reduce list to rows which contain phases and constituents
+    tdbLines = [r for r in tdbLines if any([r[:5] == 'PHASE', r[:5] == 'CONST'])]
     
-    # Extract information from phase data
-    phaseName = phaseList[0]
-    numSubl = phaseList[phaseList.index('%')+1]
-    stoich = phaseList[phaseList.index('%')+2:]
+    # Initialize data structure
+    dataStruct = []
     
-    # Add data to data structure
-    data = {'phase': phaseName,
-            'sublattices': numSubl,
-            'stoichiometry': stoich}
+    # Lines defining phase and constituents appear in pairs, with CONSTITUENT line
+    # following a PHASE line. Isolate each type and then process in pairs.
+    phases = [tdbLines[i][5:].strip() for i in range(0,len(tdbLines),2)]
+    constituents = [tdbLines[i][11:].strip() for i in range(1,len(tdbLines),2)] 
+    for p, c in zip(phases, constituents):
+        #print(p, c)
+        # Split phase information into list
+        phaseList = [i for i in p.split(' ') if len(i)>0]
+        
+        ## Extract information from phase data
+        phaseName = phaseList[0]
+        try:
+            numSubl = phaseList[phaseList.index('%')+1]
+            stoich = phaseList[phaseList.index('%')+2:]
+        except:
+            # Handle error if '%' is paired with another symbol/character
+            # Instead of finding exact match '%', find index of list item that
+            # contains '%'
+            ind = [phaseList.index(i) for i in phaseList if i.find('%')>=0][0]
+            numSubl = phaseList[ind+1]
+            stoich = phaseList[ind+2:]
+        
+        ## Extract information from constituents
+        # Split constituents using colon ":"
+        c = [i.strip() for i in c.split(':')]
+        
+        # Remove blank/empty list items
+        c = [i for i in c if len(i)>0]
+            
+        # Create a list of dictionaries for constituents
+        # May need to strip remaining '%' from list items ***
+        const = [{'constituent': i} for i in c]
+        
+        # Add data to data structure
+        data = {'phase': phaseName,
+                'sublattices': numSubl,
+                'stoichiometry': stoich,
+                'constituents': const}
+        
+        dataStruct.append(data)
     
-
-
-
-#%%    
+#%%   
+# Retrieve COST507R TDB file
+url = 'https://raw.githubusercontent.com/colin-lmr/lmr-tdc/main/tdb/cost507R.TDB'
+tdb = requests.get(url).text
+ 
+# Clean TDB for processing
 tdbLines = cleanTDB(tdb)
+
+# Retrieve data from TDB
 elems = getElements(tdbLines)
 fns = getFunctions(tdbLines)
 params = getParameters(tdbLines)
+phases = getPhases(tdbLines)
+
+# Combine data and save as TDJ, (T)hermo(D)ynamic (J)SON
+# Data structure is expected to change once computation code develops
+# Need to add data citations if using this as a file format ***
+tdj = {'elements': elems,
+       'functions': fns,
+       'parameters': params,
+       'phases': phases}
+path = 'C:\\Users\\Fletcher\\OneDrive - Longleaf Materials Research\\Documents\\Longleaf Materials Research\\Projects\\lmr-tdc\\tdb\\'
+with open(path + 'COST507R.json', 'w', encoding='utf-8') as f:
+    json.dump(tdj, f, ensure_ascii=False, indent=4)
 
 
 #%% Function time testing
@@ -207,8 +269,3 @@ def test():
 
 timeit.timeit('test()', setup='from __main__ import test', number=10000)
 
-
-#%%
-# Write JSON
-with open(path + 'COST507R.json', 'w', encoding='utf-8') as f:
-    json.dump(<name>, f, ensure_ascii=False, indent=4)
