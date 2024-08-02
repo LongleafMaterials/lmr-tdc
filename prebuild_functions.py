92317df865a510e6c0221bc5f51ea5207cd3b621
colin-lmr <colin@longleafresearch.com>
2024-08-01 21:17:08 -0400

    EOD update

---
+++ prebuild_functions.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  1 21:00:32 2024
+
+The most time-consuming parts of calculations are lookups and replacements
+associated with finding and preparing functions and nested functions. 
+
+Intent is to pre-build all functions from a TDB for faster reference
+
+
+@author: Colin-LMR
+"""
+
