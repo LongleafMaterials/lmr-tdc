2c0fb288553e9e88635754879364606a436e6cf4
colin-lmr <colin@longleafresearch.com>
2024-08-01 21:16:01 -0400

    EOD update

--- test_calc.py
+++ test_calc.py
@@ -20,65 +20,111 @@ cudf (GPU-accelerated dataframes, analagous to pandas) can be used with WSL2
 # urllib - retrieving TDB data
 # json - reading TDB data
 # pandas - data handling/access
+# re - Identification of function names within another function
 import pycuda.autoinit
 import cupy as cp
-import cutensor as ct
+#import cutensor as ct
 from urllib.request import urlopen
 import json
 import pandas as pd
+import re
+import sys
 
-# Retrieve COST507R JSON file
+# Add local working directory to PATH
+wd = 'C:\\Users\\Colin-LMR\\OneDrive - Longleaf Materials Research\\Documents\\Longleaf Materials Research\\Projects\\lmr-tdc\\'
+sys.path.append(wd)
+
+# TDC Functions
+from load_data import loadData
+
+# Path to COST507R JSON file
 url = 'https://raw.githubusercontent.com/LongleafMaterials/lmr-tdc/main/tdb/COST507R.json'
-tdb = json.loads(urlopen(url).read())
 
-# Load JSON thermo data into dataframes
-def loadData(tdb):
-    # Load element data
-    elem = pd.DataFrame(tdb['elements'])
-    
-    # Load and prepare parameter data
-    param = []
-    for i in tdb['phases']:
-        param += i['parameters']
-    param = pd.DataFrame(param)    
-    
-    # Load function data
-    func = []
-    fName = []
-    for i in tdb['functions']:
-        func += i['functions']
-        fName += len(i['functions']) * [i['name']]
-    func = pd.DataFrame(func)
-    func.insert(0, 'name', fName)
-    for c in ['min_temp', 'max_temp']:
-        func[c] = func[c].astype(float)
-    
-    return (elem, param, func)
+# Load data into dataframes
+elem, param, func = loadData(url)
 
 # Run thermo function with specified name and temperature
 def fCalc(name, T):
-    # Retrieve function matching specified name and temperature
-    f = func.loc[(func['name'] == name) & 
-                 (func['min_temp'] <= T) &
-                 (func['max_temp'] >= T)]['function'].values[0]    
+    None
 
-    
+
+
+
+
+#%% Test Section
+import time
+import os
+import numpy as np
+import re
+import math
 
 # Test variables
-name = 'GHSERBB'
+name = 'GLIQAL'
 temp = 1215
 
-# Load data into dataframes
-elem, param, func = loadData(tdb)
 
-#%%
-
-T = temp
-f = func.loc[(func['name'] == name) & 
-             (func['min_temp'] <= temp) &
-             (func['max_temp'] >= temp)]['function'].values[0]
+def method1(T):
+    try:
+        f = func.loc[(func['name'] == name) & 
+                     (func['min_temp'] <= T) &
+                     (func['max_temp'] >= T)]['function'].values[0]
+    except:
+        msg = 'No function found for ' + name + ' at ' + str(temp) + 'K'
     
-eval(f)
+    # Look for referenced functions
+    rf = re.findall(r'([A-Z]{2,})', f)
+    
+    # Substitute named function(s) into original
+    for r in rf:
+        f = f.replace(r, func.loc[(func['name'] == r) & 
+                                  (func['min_temp'] <= T) &
+                                  (func['max_temp'] >= T)]['function'].values[0])
+    
+    eval(f)
+
+
+def buildFunctions(temps):
+    functions = []
+    for T in temps:
+        try:
+            f = func.loc[(func['name'] == name) & 
+                         (func['min_temp'] <= T) &
+                         (func['max_temp'] >= T)]['function'].values[0]
+        except:
+            msg = 'No function found for ' + name + ' at ' + str(temp) + 'K'
+        
+        # Look for referenced functions
+        rf = re.findall(r'([A-Z]{2,})', f)
+        
+        # Substitute named function(s) into original
+        for r in rf:
+            f = f.replace(r, func.loc[(func['name'] == r) & 
+                                      (func['min_temp'] <= T) &
+                                      (func['max_temp'] >= T)]['function'].values[0])
+        
+        functions.append(f)
+    return functions
+       
+def method2(f):
+    eval(f)
+
+start = time.time()
+for T in range(300, 1501):
+    method1(T)
+end = time.time()
+msg = 'Method 1 elapsed time: ' + str(end-start) + ' seconds'
+print(msg)
+
+# Note - it's slightly faster (~5%) to prebuild the functions then evaluate all at once,
+# but almost all the time is spent building functions/looking up. Could be worth
+# prebuilding all of them into a separate file.
+start = time.time()
+temps = range(300, 1501)
+funcs = buildFunctions(temps)
+[method2(f) for f, T in zip(funcs, temps)]
+end = time.time()
+msg = 'Method 2 elapsed time: ' + str(end-start) + ' seconds'
+print(msg)
 
 ### Equilibrium for binary system
 # For a binary system:
